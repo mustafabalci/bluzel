322ad6169da827eba372c21b0351240c354a40fe
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-10 00:22:30 +0300

    screens folder, index file created

---
+++ src/screens/index.js
@@ -0,0 +1,11 @@
+import ClothesScreen from './ClothesScreen';
+import CombineScreen from './CombineScreen';
+import FeedScreen from './FeedScreen';
+import WardrobeScreen from './WardrobeScreen';
+
+export default {
+  ClothesScreen,
+  CombineScreen,
+  FeedScreen,
+  WardrobeScreen,
+};
