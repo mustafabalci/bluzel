5ef33f8db29d9ea4aac0c084434a92776aa0fb4f
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-09 23:46:14 +0300

    tab icons declared

---
+++ src/constants/icons.js
@@ -0,0 +1,11 @@
+const wardrobe_icon = require('../images/wardrobe.png');
+const clothes_icon = require('../images/clothes.png');
+const combine_icon = require('../images/combine.png');
+const feed_icon = require('../images/feed.png');
+
+export default {
+  wardrobe_icon,
+  clothes_icon,
+  combine_icon,
+  feed_icon,
+};
