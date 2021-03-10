0399bd105b9f5b035e6b4a8b9f44c28415007193
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-10 22:15:07 +0300

    onboarding screen logic implemented

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/navigation/tabs';
 import OnboardingScreen from './src/screens/OnboardingScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ActivityIndicator } from 'react-native';
+import LoginSceen from './src/screens/LoginScreen';
 
 const App = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then((value) => {
+      if (value === null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
   const Stack = createStackNavigator();
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Tabs} />
-        <Stack.Screen name="Notifications" component={OnboardingScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  if (isFirstLaunch === null) {
+    return <ActivityIndicator />;
+  } else if (setIsFirstLaunch === true) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen name="Notifications" component={OnboardingScreen} />
+          <Stack.Screen name="Home" component={Tabs} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return <LoginSceen />;
+  }
 };
 
 export default App;
--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Dimensions } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 
-const OnboardingScreen = () => {
+const width = Dimensions.get('window').width;
+
+const OnboardingScreen = ({ navigation }) => {
   return (
     <>
       <Onboarding
+        skipLabel="Geç"
+        nextLabel="Devam Et"
+        onSkip={() => navigation.navigate('Home')}
+        onDone={() => navigation.navigate('Home')}
         pages={[
           {
             backgroundColor: '#fcda05',
             image: <Image source={require('../images/ob-0.png')} />,
             title: 'Hoşgeldin 💖',
-            subtitle:
-              'Hemen gardroplarını oluşturmaya, kıyafetlerini kategorize etmeye ve kombinler oluşturmaya başlayabilirsin.',
+            subtitle: 'Uygulamamızı kısaca tanıtmamızı ister misin?',
+          },
+          {
+            backgroundColor: '#fcda05',
+            image: <Image source={require('../images/ob-1.png')} />,
+            title: 'Kıyafetlerinin Fotoğrafını Çek',
+            subtitle: 'Kıyafetlerini fotoğraflayıp kategorize edebilirsin.',
+          },
+          {
+            backgroundColor: '#fcda05',
+            image: <Image source={require('../images/ob-2.png')} />,
+            title: 'Gardroplar Oluştur',
+            subtitle: 'Kıyafetlerini farklı gardroplarda toplayabilirsin.',
+          },
+          {
+            backgroundColor: '#fcda05',
+            image: <Image source={require('../images/ob-3.png')} />,
+            title: 'Kombinler Hazırla',
+            subtitle: 'Kombinler hazırlayıp arkadaşlarınla paylaşabilirsin.',
           },
         ]}
       />
