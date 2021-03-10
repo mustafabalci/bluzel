4fffdcdf2bb6b857fd522222adece634d7ccb33f
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-10 08:54:49 +0300

    plus icon declared

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -2,10 +2,12 @@ const wardrobe_icon = require('../images/wardrobe.png');
 const clothes_icon = require('../images/clothes.png');
 const combine_icon = require('../images/combine.png');
 const feed_icon = require('../images/feed.png');
+const plus_icon = require('../images/plus.png');
 
 export default {
   wardrobe_icon,
   clothes_icon,
   combine_icon,
   feed_icon,
+  plus_icon,
 };
