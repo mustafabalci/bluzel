057ef84452a9b20af895eb49995783920c4fc501
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-10 22:13:18 +0300

    fix typo

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const LoginSceen = () => {
+const LoginScreen = () => {
   return (
     <View>
       <Text>LoginScreen</Text>
@@ -9,4 +9,4 @@ const LoginSceen = () => {
   );
 };
 
-export default LoginSceen;
+export default LoginScreen;
