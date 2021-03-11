017fb438f658d1b12d3af40baa2ea1454019970b
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-11 14:46:44 +0300

    basic auth logic created

---
+++ src/navigation/AuthProvider.js
@@ -0,0 +1,64 @@
+import React, { createContext, useState } from 'react';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+// import { GoogleSignin } from '@react-native-community/google-signin';
+// import { LoginManager, AccessToken } from 'react-native-fbsdk';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        login: async (email, password) => {
+          try {
+            await auth().signInWithEmailAndPassword(email, password);
+          } catch (e) {
+            console.log(e);
+          }
+        },
+        register: async (email, password) => {
+          try {
+            await auth()
+              .createUserWithEmailAndPassword(email, password)
+              .then(() => {
+                firestore()
+                  .collection('users')
+                  .doc(auth().currentUser.uid)
+                  .set({
+                    firstName: '',
+                    lastName: '',
+                    email: email,
+                    createdAt: firestore.Timestamp.fromDate(new Date()),
+                    userImg: null,
+                  })
+                  .catch((error) => {
+                    console.log(
+                      'Something went wrong with added user to firestore: ',
+                      error,
+                    );
+                  });
+              })
+              .catch((error) => {
+                console.log('Something went wrong with sign up: ', error);
+              });
+          } catch (e) {
+            console.log(e);
+          }
+        },
+        logout: async () => {
+          try {
+            await auth().signOut();
+          } catch (e) {
+            console.log(e);
+          }
+        },
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,128 @@
+import React, { useContext, useState } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import AuthInput from '../components/AuthInput';
+import AuthFormButton from '../components/AuthFormButton';
+import SocialButton from '../components/SocialButton';
+import { AuthContext } from '../navigation/AuthProvider';
+
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [buttonLoader, setButtonLoader] = useState(false);
+
+  const { register } = useContext(AuthContext);
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.text}>Hesap oluştur</Text>
+
+      <AuthInput
+        labelValue={email}
+        onChangeText={(userEmail) => setEmail(userEmail)}
+        placeholder="E-posta"
+        iconName="email-outline"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+
+      <AuthInput
+        labelValue={password}
+        onChangeText={(userPassword) => setPassword(userPassword)}
+        placeholder="Parola"
+        iconName="lock-open-outline"
+        secureTextEntry={true}
+      />
+
+      <AuthInput
+        labelValue={confirmPassword}
+        onChangeText={(userPassword) => setConfirmPassword(userPassword)}
+        placeholder="Parolayı onayla"
+        iconName="lock-open-outline"
+        secureTextEntry={true}
+      />
+
+      <AuthFormButton
+        text="Kayıt Ol"
+        onPress={() => register(email, password)}
+      />
+
+      <View style={styles.textPrivate}>
+        <Text style={styles.color_textPrivate}>
+          By registering, you confirm that you accept our{' '}
+        </Text>
+        <TouchableOpacity onPress={() => alert('Terms Clicked!')}>
+          <Text style={[styles.color_textPrivate, { color: '#e88832' }]}>
+            Terms of service
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.color_textPrivate}> and </Text>
+        <Text style={[styles.color_textPrivate, { color: '#e88832' }]}>
+          Privacy Policy
+        </Text>
+      </View>
+
+      <View>
+        <SocialButton
+          text="Google ile Kayıt Ol"
+          iconName="google"
+          color="#de4d41"
+          bgColor="#f5e7ea"
+          onPress={() => alert('google')}
+        />
+      </View>
+
+      <TouchableOpacity
+        style={styles.navButton}
+        onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.navButtonText}>Üyeliğin var mı? Giriş Yap.</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+};
+
+export default SignupScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f9fafd',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  text: {
+    fontFamily: 'Kufam-SemiBoldItalic',
+    fontSize: 28,
+    marginBottom: 10,
+    color: '#051d5f',
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#2e64e5',
+    fontFamily: 'Lato-Regular',
+  },
+  textPrivate: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginVertical: 35,
+    justifyContent: 'center',
+  },
+  color_textPrivate: {
+    fontSize: 13,
+    fontWeight: '400',
+    fontFamily: 'Lato-Regular',
+    color: 'grey',
+  },
+});
