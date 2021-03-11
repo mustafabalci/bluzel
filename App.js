a09662f4494acb4de353d6acb4f1ff426f637b5f
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-11 14:47:12 +0300

    app return navigation providers

--- App.js
+++ App.js
@@ -1,42 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import Tabs from './src/navigation/tabs';
-import OnboardingScreen from './src/screens/OnboardingScreen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ActivityIndicator } from 'react-native';
-import LoginScreen from './src/screens/LoginScreen';
+import React from 'react';
+import Providers from './src/navigation';
 
 const App = () => {
-  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
-
-  useEffect(() => {
-    AsyncStorage.getItem('alreadyLaunched').then((value) => {
-      if (value === null) {
-        AsyncStorage.setItem('alreadyLaunched', 'true');
-        setIsFirstLaunch(true);
-      } else {
-        setIsFirstLaunch(false);
-      }
-    });
-  }, []);
-
-  const Stack = createStackNavigator();
-
-  if (isFirstLaunch === null) {
-    return <ActivityIndicator />;
-  } else if (setIsFirstLaunch === true) {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator headerMode="none">
-          <Stack.Screen name="Notifications" component={OnboardingScreen} />
-          <Stack.Screen name="Home" component={Tabs} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  } else {
-    return <LoginScreen />;
-  }
+  return <Providers />;
 };
 
 export default App;
