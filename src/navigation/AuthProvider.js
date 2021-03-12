1d276dee68e9e574530462d3bdd9f367892afeae
Mustafa BALCI <balcimustafa1@gmail.com>
2021-03-12 12:32:24 +0300

    form validation added

--- src/navigation/AuthProvider.js
+++ src/navigation/AuthProvider.js
@@ -9,7 +9,16 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const { showLoader, hideLoader } = useContext(LoaderContext);
+  const { showLoader, hideLoader, showToast } = useContext(LoaderContext);
+
+  const validateEmail = (email) => {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 6;
+  };
 
   return (
     <AuthContext.Provider
@@ -18,44 +27,62 @@ export const AuthProvider = ({ children }) => {
         setUser,
         login: async (email, password) => {
           showLoader();
-          try {
-            await auth().signInWithEmailAndPassword(email, password);
+
+          if (validateEmail(email) && validatePassword(password)) {
+            try {
+              await auth().signInWithEmailAndPassword(email, password);
+              hideLoader();
+            } catch (e) {
+              console.log(e);
+              hideLoader();
+              showToast('E-Posta veya parola yanlış.');
+            }
+          } else {
             hideLoader();
-          } catch (e) {
-            console.log(e);
-            hideLoader();
+            showToast('E-Posta veya parola yanlış.');
           }
         },
         register: async (email, password) => {
           showLoader();
-          try {
-            await auth()
-              .createUserWithEmailAndPassword(email, password)
-              .then(() => {
-                hideLoader();
-                firestore()
-                  .collection('users')
-                  .doc(auth().currentUser.uid)
-                  .set({
-                    firstName: '',
-                    lastName: '',
-                    email: email,
-                    createdAt: firestore.Timestamp.fromDate(new Date()),
-                    userImg: null,
-                  })
-                  .catch((error) => {
-                    hideLoader();
-                    console.log(
-                      'Something went wrong with added user to firestore: ',
-                      error,
-                    );
-                  });
-              })
-              .catch((error) => {
-                console.log('Something went wrong with sign up: ', error);
-              });
-          } catch (e) {
-            console.log(e);
+          if (validateEmail(email) && validatePassword(password)) {
+            try {
+              await auth()
+                .createUserWithEmailAndPassword(email, password)
+                .then(() => {
+                  hideLoader();
+                  firestore()
+                    .collection('users')
+                    .doc(auth().currentUser.uid)
+                    .set({
+                      firstName: '',
+                      lastName: '',
+                      email: email,
+                      createdAt: firestore.Timestamp.fromDate(new Date()),
+                      userImg: null,
+                    })
+                    .catch((error) => {
+                      hideLoader();
+                      console.log(
+                        'Something went wrong with added user to firestore: ',
+                        error,
+                      );
+                      showToast('Kayıt olurken bir hata meydana geldi.');
+                    });
+                })
+                .catch((error) => {
+                  console.log('Something went wrong with sign up: ', error);
+                  showToast('Kayıt olurken bir hata meydana geldi.');
+                });
+            } catch (e) {
+              console.log(e);
+              showToast('Kayıt olurken bir hata meydana geldi.');
+              hideLoader();
+            }
+          } else {
+            showToast(
+              'Lütfen geçerli bir e-posta adresi ve en az 6 haneli bir parola girin.',
+            );
+            hideLoader();
           }
         },
         logout: async () => {
